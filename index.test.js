8130d2cd3cda1244b9b66a69e0cea01fdcc48337
Geoffrey Lessel <code@geoffreylessel.com>
2022-04-18 14:33:13 -0500

    chore: add eslint/prettier and run it

--- index.test.js
+++ index.test.js
@@ -1,100 +1,100 @@
-const pcoUrl = require("./");
+const pcoUrl = require("./")
 
 describe("When incomplete arguments given", () => {
   test("returns undefined when no arguments are given", () => {
-    expect(pcoUrl()()).toBeUndefined();
-  });
+    expect(pcoUrl()()).toBeUndefined()
+  })
 
   test("returns undefined when only environment is given", () => {
-    expect(pcoUrl("production")()).toBeUndefined();
-    expect(pcoUrl("staging")()).toBeUndefined();
-    expect(pcoUrl("development")()).toBeUndefined();
-    expect(pcoUrl("test")()).toBeUndefined();
-  });
+    expect(pcoUrl("production")()).toBeUndefined()
+    expect(pcoUrl("staging")()).toBeUndefined()
+    expect(pcoUrl("development")()).toBeUndefined()
+    expect(pcoUrl("test")()).toBeUndefined()
+  })
 
   test("returns undefined when product name is given", () => {
-    expect(pcoUrl()("sample-app-name")).toBeUndefined();
-  });
-});
+    expect(pcoUrl()("sample-app-name")).toBeUndefined()
+  })
+})
 
 describe("When no domain inferred", () => {
   describe("and given environment 'production'", () => {
     test("returns product url using 'https://{product-name}.planningcenteronline.com' scheme", () => {
       expect(pcoUrl("production")("accounts")).toBe(
         "https://accounts.planningcenteronline.com"
-      );
-    });
-  });
+      )
+    })
+  })
 
   describe("and given environment 'staging'", () => {
     test("returns product url using 'https://{product-name}-staging.planningcenteronline.com' scheme", () => {
       expect(pcoUrl("staging")("accounts")).toBe(
         "https://accounts-staging.planningcenteronline.com"
-      );
-    });
-  });
-});
+      )
+    })
+  })
+})
 
 describe("When inferred domain is 'planningcenteronline.com'", () => {
-  let previousLocation = window.location;
+  let previousLocation = window.location
 
   beforeEach(() => {
-    delete window.location;
-    window.location = new URL("https://planningcenteronline.com");
-  });
+    delete window.location
+    window.location = new URL("https://planningcenteronline.com")
+  })
 
   afterEach(() => {
-    delete window.location;
-    window.location = previousLocation;
-  });
+    delete window.location
+    window.location = previousLocation
+  })
 
   describe("and given environment 'production'", () => {
     test("returns product url using 'https://{product-name}.planningcenteronline.com' scheme", () => {
-      pcoUrl("development");
+      pcoUrl("development")
       expect(pcoUrl("production")("accounts")).toBe(
         "https://accounts.planningcenteronline.com"
-      );
-    });
-  });
+      )
+    })
+  })
 
   describe("and given environment 'staging'", () => {
     test("returns product url using 'https://{product-name}-staging.planningcenteronline.com' scheme", () => {
       expect(pcoUrl("staging")("accounts")).toBe(
         "https://accounts-staging.planningcenteronline.com"
-      );
-    });
-  });
-});
+      )
+    })
+  })
+})
 
 describe("When inferred domain is 'planningcenter.com'", () => {
-  let previousLocation = window.location;
+  let previousLocation = window.location
 
   beforeEach(() => {
-    delete window.location;
-    window.location = new URL("https://planningcenter.com");
-  });
+    delete window.location
+    window.location = new URL("https://planningcenter.com")
+  })
 
   afterEach(() => {
-    delete window.location;
-    window.location = previousLocation;
-  });
+    delete window.location
+    window.location = previousLocation
+  })
 
   describe("and given environment 'production'", () => {
     test("returns product url using 'https://{product-name}.planningcenter.com' scheme", () => {
       expect(pcoUrl("production")("accounts")).toBe(
         "https://accounts.planningcenter.com"
-      );
-    });
-  });
+      )
+    })
+  })
 
   describe("and given environment 'staging'", () => {
     test("returns product url using 'https://{product-name}-staging.planningcenter.com' scheme", () => {
       expect(pcoUrl("staging")("accounts")).toBe(
         "https://accounts-staging.planningcenter.com"
-      );
-    });
-  });
-});
+      )
+    })
+  })
+})
 
 /*
 The develompent tests flip the concern order from `production|staging` intentionally.
@@ -103,71 +103,67 @@ They are both dynamic buth different in their approach.
 `development` is open-ended and supports any tld.
 */
 describe("When given environment 'development'", () => {
-  let previousLocation = window.location;
+  let previousLocation = window.location
 
   describe("and inferred domain is pco.codes", () => {
     beforeEach(() => {
-      delete window.location;
-      window.location = new URL("http://pco.codes");
-    });
+      delete window.location
+      window.location = new URL("http://pco.codes")
+    })
 
     afterEach(() => {
-      delete window.location;
-      window.location = previousLocation;
-    });
+      delete window.location
+      window.location = previousLocation
+    })
 
     test("returns product url using 'https://{product-name}.pco.codes' scheme", () => {
       expect(pcoUrl("development")("accounts")).toBe(
         "http://accounts.pco.codes"
-      );
-    });
-  });
+      )
+    })
+  })
 
   describe("and inferred domain is pco.test", () => {
     beforeEach(() => {
-      delete window.location;
-      window.location = new URL("http://pco.test");
-    });
+      delete window.location
+      window.location = new URL("http://pco.test")
+    })
 
     afterEach(() => {
-      delete window.location;
-      window.location = previousLocation;
-    });
+      delete window.location
+      window.location = previousLocation
+    })
 
     test("returns product url using 'https://{product-name}.pco.test' scheme", () => {
-      expect(pcoUrl("development")("accounts")).toBe(
-        "http://accounts.pco.test"
-      );
-    });
-  });
+      expect(pcoUrl("development")("accounts")).toBe("http://accounts.pco.test")
+    })
+  })
 
   describe("and inferred domain is localhost", () => {
     beforeEach(() => {
-      delete window.location;
-      window.location = new URL("http://localhost");
-    });
+      delete window.location
+      window.location = new URL("http://localhost")
+    })
 
     afterEach(() => {
-      delete window.location;
-      window.location = previousLocation;
-    });
+      delete window.location
+      window.location = previousLocation
+    })
 
     test("returns product url using 'https://{product-name}.pco.test' scheme", () => {
-      expect(pcoUrl("development")("accounts")).toBe(
-        "http://accounts.pco.test"
-      );
-    });
-  });
-});
+      expect(pcoUrl("development")("accounts")).toBe("http://accounts.pco.test")
+    })
+  })
+})
 
 describe("When given environment 'test'", () => {
   test("development url", () => {
-    expect(pcoUrl("test")("accounts")).toBe("http://accounts.pco.test");
-  });
-});
+    expect(pcoUrl("test")("accounts")).toBe("http://accounts.pco.test")
+  })
+})
 
 describe("Assorted smoke-tests", () => {
-  let previousLocation = window.location;
+  let previousLocation = window.location
 
   test.each([
     [
@@ -211,13 +207,13 @@ describe("Assorted smoke-tests", () => {
   ])(
     "product %p, given %p, under domain %p",
     (product, environment, domain, expected) => {
-      delete window.location;
-      window.location = new URL(domain);
+      delete window.location
+      window.location = new URL(domain)
 
-      expect(pcoUrl(environment)(product)).toBe(expected);
+      expect(pcoUrl(environment)(product)).toBe(expected)
 
-      delete window.location;
-      window.location = previousLocation;
+      delete window.location
+      window.location = previousLocation
     }
-  );
-});
+  )
+})
